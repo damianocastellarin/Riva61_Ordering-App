dee0eb1fc39f79f40efc1c5ff8e8d55fdd7f3c76
Damiano Castellarin <damianocastellarin@gmail.com>
2026-03-02 23:03:07 +0100

    remove automatic upgrade

--- service-worker.js
+++ service-worker.js
@@ -15,52 +15,37 @@ const urlsToCache = [
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
+
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('SW: Creazione nuova cache', CACHE_NAME);
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('SW: Eliminazione vecchia cache', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
       );
-    }).then(() => {
-      return self.clients.claim();
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET' || !event.request.url.startsWith('http')) return;
+  if (event.request.method !== 'GET') return;
 
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        if (response && response.status === 200) {
-          const resClone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
-        }
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
         return response;
       })
       .catch(() => caches.match(event.request))
   );
-});
-
-self.addEventListener('message', event => {
-  if (event.data === 'skipWaiting') {
-    self.skipWaiting();
-  }
 });
