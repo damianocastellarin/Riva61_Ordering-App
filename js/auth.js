0963d8ab83b1c7907c83572e0cc7b787fdeda0f3
Damiano Castellarin <damianocastellarin@gmail.com>
2026-03-10 21:19:05 +0100

    add spinner

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,5 @@
+import { ui } from './ui.js';
+
 const loginContainer = document.getElementById('login-container');
 const appContent = document.getElementById('app-content');
 const adminContent = document.getElementById('admin-content');
@@ -36,37 +38,46 @@ window.fb.onAuthStateChanged(window.fb.auth, async (user) => {
             }
         } catch (error) {
             console.error("Errore durante il recupero del ruolo:", error);
+        } finally {
+            ui.hideLoader();
         }
     } else {
         loginContainer.classList.remove('hidden');
         appContent.classList.add('hidden');
         adminContent.classList.add('hidden');
         sessionStorage.removeItem("ordine_bar_salvato");
+        ui.hideLoader();
     }
 });
 
-loginBtn.addEventListener('click', async () => {
-    const email = document.getElementById('login-email').value;
-    const password = document.getElementById('login-password').value;
-    
-    if (!email || !password) return alert("Inserisci email e password");
+if (loginBtn) {
+    loginBtn.addEventListener('click', async () => {
+        const email = document.getElementById('login-email').value;
+        const password = document.getElementById('login-password').value;
+        
+        if (!email || !password) return;
 
-    try {
-        await window.fb.signInWithEmailAndPassword(window.fb.auth, email, password);
-    } catch (e) {
-        console.error(e);
-        alert("Accesso fallito: credenziali errate.");
-    }
-});
+        ui.showLoader();
+        try {
+            await window.fb.signInWithEmailAndPassword(window.fb.auth, email, password);
+        } catch (e) {
+            console.error(e);
+            ui.hideLoader();
+            alert("Accesso fallito: credenziali errate.");
+        }
+    });
+}
 
 document.addEventListener('click', async (e) => {
     if (e.target.id === 'logoutBtn' || e.target.id === 'logoutAdminBtn') {
         if (confirm("Vuoi uscire dall'account?")) {
+            ui.showLoader();
             try {
                 await window.fb.signOut(window.fb.auth);
                 window.location.reload();
             } catch (error) {
                 console.error("Errore Logout:", error);
+                ui.hideLoader();
             }
         }
     }
